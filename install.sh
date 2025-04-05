41a33e9e1b70394ea3fa1fea249dfaaa1ccfaec8
darkviper-dev <darkviper314@gmail.com>
2025-04-05 05:08:19 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 
 ROOT_UID=0
 THEME_DIR="/usr/share/grub/themes"
-THEME_NAME=chaos
+THEME_NAME=dark
 
 MAX_DELAY=20                                        # max delay for user to enter root password
 
